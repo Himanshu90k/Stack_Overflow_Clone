989ffd3e563a28700aac7c04841ed6fc85b1c760
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-12-11 12:42:50 +0530

    Question Page - 2

--- client/src/pages/Question/Questiondetails.tsx
+++ client/src/pages/Question/Questiondetails.tsx
@@ -6,12 +6,53 @@ import downvote from '../../assets/sort-down.svg';
 import './Question.css';
 import Avatar from "../../Components/Avatar/Avatar";
 import Displayanswer from "./Displayanswer";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Questiondetails = () => {
 
     const [answer, setAnswer] = useState("");
     const questionlist = null;
+    const user = null;
+    const location = useLocation();
+    const navigate = useNavigate();
+    const url = "http://localhost:3000";
+
+    const handlepostans = (e, answerlength) => {
+        e.preventDefault();
+        if(user === null) {
+            alert("Login or Signup to answer a question");
+            navigate('/Auth');
+        } else {
+            if(answer === '') {
+                alert("Enter an answer before submitting");
+            } else {
+                setAnswer("");
+            }
+        }
+    };
+
+    const handleshare = () => {
+        copy(url + location.pathname);
+        alert("Copied url :" + url + location.pathname);
+    };
+
+    const handleDelete = () => {
+
+    };
+
+    const handleupvote = () => {
+        if(user === null) {
+            alert("Login or Signup to answer a question");
+            navigate('/Auth');
+        }
+    };
+
+    const handledownvote = () => {
+        if(user === null) {
+            alert("Login or Signup to answer a question");
+            navigate('/Auth');
+        }
+    };
 
     return (
         <div className="question-details-page">
@@ -46,10 +87,45 @@ const Questiondetails = () => {
                                         </div>
                                         <div>
                                             <p>Asked {moment(question.askedon).fromNow()}</p>
-                                            <Link></Link>
+                                            <Link to={`Users/${question.userid}`} className="user-link" style={{color: "#0086d8"}}>
+                                                <Avatar backgroundColor={"orange"} px="8px" py="5px" borderRadius="4px">
+                                                    {question.userposted.charAt(0).toUpperCase()}
+                                                </Avatar>
+                                                <div>{question.userposted}</div>
+                                            </Link>
                                         </div>
                                     </div>
                                 </div>
+                            </section>
+                            {question.noofanswers !== 0 && (
+                                <section>
+                                    <h3>{question.noofanswers} Answers</h3>
+                                    <Displayanswer key={question._id} question={question} handleshare={handleshare}/>
+                                </section>
+                            )}
+                            <section className="post-ans-container">
+                                <h3>Your Answer</h3>
+                                <form onSubmit={((e) => {
+                                    handlepostans(e, question.answer.length)
+                                })}>
+                                    <textarea name="" id="" cols={30} rows={10} value={answer} onChange={(e) => setAnswer(e.target.value)}></textarea>
+                                <br />
+                                <input type="submit" className="post-ans-btn" value="Post your Answer"/>
+                                </form>
+                                <p>
+                                    Browse other Question tagged
+                                    {question.questiontags.map((tag) => (
+                                        <Link to='/Tags' key={tag} className="ans-tag">
+                                            {" "}
+                                            {tag}{" "}
+                                        </Link>
+                                    ))}{" "}
+                                    or
+                                    <Link to="/Askquestion" style={{textDecoration: "none", color: "#009dff"}}>
+                                        {" "}
+                                        Ask your own question
+                                    </Link>
+                                </p>
                             </section>
                         </div>
                     ))}
