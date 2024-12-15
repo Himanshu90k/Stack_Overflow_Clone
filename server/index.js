c45f33e280aaac7943a32acea5dacf9064c8f859
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-12-15 22:00:13 +0530

    setup server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).json({"message": "server is running."});
+});
+
+const PORT = process.env.PORT || 6000;
+const DB_URI = process.env.DB_URI;
+
+mongoose.connect(DB_URI , {dbName: 'StackOverflowData'})
+    .then(() => app.listen(PORT, () => console.log(`server is running on port ${PORT}`)))
+    .catch((err) => console.log(err.message))
