f9c33ae231d633b82326d31f14f2f7072b27feca
Himanshu Rawat <himanshurawat90k@gmail.com>
2025-01-02 21:57:26 +0530

    fix authSlice

--- client/src/state/auth/authSlice.ts
+++ client/src/state/auth/authSlice.ts
@@ -1,7 +1,8 @@
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import * as api from '../../api';
 import { setCurrentUser } from "../currentUser/currentUserSlice";
 import { fetchUsersAsync } from "../users/usersSlice";
+import { AppDispatch } from "../store";
 
 interface AuthState {
     result: {
@@ -12,62 +13,83 @@ interface AuthState {
     token?: string;
 };
 
-const initialState: AuthState = {result: {name: '', email: '', password: ''}, token: ''};
+const initialState: AuthState = { result: { name: '', email: '', password: '' }, token: '' };
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {},
-    extraReducers (builder) {
+    extraReducers(builder) {
         builder
-        .addCase(signupAsync.fulfilled, (state, action: PayloadAction<{result: AuthState["result"], token: AuthState["token"]}>) => {
-            localStorage.setItem('Profile', JSON.stringify(action.payload));
-            state.result = action.payload.result;
+            .addCase(signupAsync.fulfilled, (state, action) => {
+                state.result = action.payload.data.result;
+                state.token = action.payload.data.token;
+                action.payload.navigate('/');
+            })
+            .addCase(signupAsync.rejected, (_, action) => {
+                console.log(action.error);
+            });
 
+        builder
+            .addCase(loginAsync.fulfilled, (state, action) => {
+                state.result = action.payload.data.result;
+                state.token = action.payload.data.token;
+                action.payload.navigate('/');
+            })
+            .addCase(loginAsync.rejected, (_, action) => {
+                console.log(action.error);
+            });
+    },
+
+});
+
+export const signupAsync = createAsyncThunk<
+    {data: AuthState, navigate: (path: string) => void}, // Return type
+    { name: string; email: string; password: string; navigate: (path: string) => void }, // Argument type
+    { dispatch: AppDispatch }
+>(
+    'auth/signupAsync',
+    async ({ name, email, password, navigate }, thunkApi) => {
+
+        const data = { name, email, password };
+        const response = await api.signup(data);
+
+        const { dispatch } = thunkApi;
+        if (response.status === 200) {
+
+            localStorage.setItem('Profile', JSON.stringify(response.data));
             const profile = localStorage.getItem('Profile');
-            if(profile) {
+            if (profile) {
                 dispatch(setCurrentUser(JSON.parse(profile)));
             };
 
             dispatch(fetchUsersAsync());
             navigate('/');
-        })
-        .addCase(signupAsync.rejected, (_, action) => {
-            console.log(action.error);
-        });
-
-        builder
-        .addCase(loginAsync.fulfilled, (state, action: PayloadAction<AuthState>) => {
-            localStorage.setItem('Profile', JSON.stringify(action.payload));
-            state.result = action.payload.result;
-
-            const profile = localStorage.getItem('Profile');
-            if(profile) {
-                dispatch(setCurrentUser(JSON.parse(profile)));
-            };
-
-            navigate('/');
-        })
-        .addCase(loginAsync.rejected, (_, action) => {
-            console.log(action.error);
-        });
-    },
-
-});
-
-export const signupAsync = createAsyncThunk(
-    'auth/signupAsync',
-    async (data: {name: string, email: string, password: string}) => {
-        const response = await api.signup(data);
-        return response.data;
+        }
+        return {data: response.data, navigate};
     },
 );
 
-export const loginAsync = createAsyncThunk(
+export const loginAsync = createAsyncThunk<
+    {data: AuthState, navigate: (path: string) => void }, // Return type
+    { email: string; password: string; navigate: (path: string) => void }, // Argument type
+    { dispatch: AppDispatch }
+>(
     'auth/loginAsync',
-    async (data: {email: string, password: string}) => {
+    async ({ email, password, navigate }, thunkApi) => {
+
+        const data = { email, password };
         const response = await api.login(data);
-        return response.data;
+
+        const { dispatch } = thunkApi;
+        if (response.status === 200) {
+            localStorage.setItem('Profile', JSON.stringify(response.data));
+            const profile = localStorage.getItem('Profile');
+            if (profile) {
+                dispatch(setCurrentUser(JSON.parse(profile)));
+            };
+        }
+        return {data: response.data, navigate};
     },
 );
 
